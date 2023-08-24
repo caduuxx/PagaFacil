b5216a9ad9bd586b635ece9ce0afa0b86b855b5d
Carlos Eduardo Brandao Rodrigues <cbcadulebe@dinfor.uniceub>
2023-08-24 09:24:51 -0300

    dia 24-08

---
+++ C#/Program.cs
@@ -0,0 +1,33 @@
+﻿using AppBackPagaFacilV0._1.dominio;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SomarBoleto SomarBoleto = new SomarBoleto();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o valor dos boletos que vc deseja pagar");
+            int valorBoleto = int.Parse(Console.ReadLine());
+
+            double[] contas = new double[valorBoleto];
+
+            for (int i = 0; i < valorBoleto; i++)
+            {
+                Console.WriteLine($"Qual o valor da conta {i + 1}: ");
+                contas[i] = double.Parse(Console.ReadLine());
+
+            }
+             double total = CalcularSomaBoleto(contas);
+            
+            Console.WriteLine($"O total dos boletos a serem pagos é : {total:C}");
+
+        }
+    }
+
+    private static double CalcularSomaBoleto(double[] contas)
+    {
+        throw new NotImplementedException();
+    }
+}
