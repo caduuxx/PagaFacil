2f1fc7a44b848ce19318675775308aa7c58766b5
FranciscoLeonardoOMaia <141328619+FranciscoLeonardoOMaia@users.noreply.github.com>
2023-09-21 08:46:17 -0300

    Add files via upload

---
+++ PagaFacil-Versao0.2.3-master/C#/dominio/SomarBoleto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBackPagaFacilV0._1.dominio
+{
+    internal class SomarBoleto
+    {
+        public SomarBoleto() { }
+
+        static double CaulcularSomaBoleto(double[] contas)
+        {
+            double soma = 0.0;
+
+            foreach (double conta in contas)
+            {
+                soma += conta;
+            }
+            return soma;
+        }
+    }
+}
