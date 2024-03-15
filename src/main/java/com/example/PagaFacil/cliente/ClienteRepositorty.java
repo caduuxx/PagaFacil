bca6f0e54ef74d8521a3b8cfddd85e4ff4bff764
caduuxx <115509827+caduuxx@users.noreply.github.com>
2024-03-15 09:18:41 -0300

    Delete src directory

--- src/main/java/com/example/PagaFacil/cliente/ClienteRepositorty.java
+++
@@ -1,7 +0,0 @@
-package com.example.PagaFacil.cliente;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepositorty extends JpaRepository<Cliente,Long> {
-
-}
--- src/main/java/com/example/PagaFacil/cliente/ClienteRequestDTO.java
+++
@@ -1,6 +0,0 @@
-package com.example.PagaFacil.cliente;
-
-public record ClienteRequestDTO(Long id, String cpf,String cnpj,String nomeSocial,
-                                String email,String senha,String telefone,String endereco) {
-
-}
